a7dd8e932f0ff11eb0c2270a45555634d16b0aaf
Diocleniax789 <julioramdom123@gmail.com>
2024-10-05 00:00:06 -0300

    Se ha añadido un menu. Lo unico que falta es centrarlo, pero no estaria funcionando al darle las coordenadas

--- calculadora-formal.c
+++ calculadora-formal.c
@@ -4,13 +4,13 @@
 
 void presentacion();
 void gotoxy(int,int);
+void menu_principal();
+void calculadora_comun();
+void calculadora_cientifica();
 
 int main(){
     presentacion();
-
-
-
-
+    menu_principal();
     return 0;
 }
 
@@ -29,3 +29,44 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void menu_principal(){
+    int op;
+
+    do{
+        system("cls");
+        gotoxy(50,0);
+        printf("\n ||||||||||||||||||||||||||| \n");
+        printf("\n || FORMAL CALCULATOR 1.0 || \n");
+        printf("\n ||||||||||||||||||||||||||| \n");
+        printf("\n");
+        printf("\n * MENU PRINCIPAL *  \n");
+        printf("\n ------------------- \n");
+        printf("\n");
+        printf("\n ================================== \n");
+        printf("\n |1| -    Calculadora comun   - |1| \n");
+        printf("\n |2| - Calculadora cientifica - |2| \n");
+        printf("\n |3| -          Salir         - |3| \n");
+        printf("\n ================================== \n");
+        printf("\n");
+        do{
+            fflush(stdin);
+            printf("Seleccione opcion: ");
+            scanf("%i",&op);
+            if(op < 1 || op > 3){
+                printf("\n xx ERROR. INGRESE UNA OPCION VALIDA xx \n");
+            }
+        } while(op < 1 || op > 3);
+        switch(op){
+            case 1:
+                system("cls");
+                // calculadora_comun();
+            break;
+
+            case 2:
+                system("cls");
+               // calculadora_cientifica();
+            break;
+        }
+
+    } while(op != 3);
+}

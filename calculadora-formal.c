d5fe24d5e0b6c703161e51750fe1bf4d99b3d72f
Diocleniax789 <julioramdom123@gmail.com>
2024-10-04 23:31:27 -0300

    Se ha añadido la presentacion antes de iniciar el programa. Luego una funcion para obtener las coordenas

--- calculadora-formal.c
+++ calculadora-formal.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+
+void presentacion();
+void gotoxy(int,int);
+
+int main(){
+    presentacion();
+
+
+
+
+    return 0;
+}
+
+void presentacion(){
+gotoxy(50,4);
+printf("FORMAL CALCULATOR 1.0");
+printf("\n\n");
+gotoxy(43, 10);
+system("pause");
+}
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 

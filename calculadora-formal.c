faa4f0d9b589f5185a7855d3f0ca0ad2d83e706b
Diocleniax789 <julioramdom123@gmail.com>
2024-10-07 01:53:05 -0300

    se esta haciendo el proceso de del strtok

--- calculadora-formal.c
+++ calculadora-formal.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define FILAS 5
-#define COLUMNAS 3
-
 void presentacion();
 void gotoxy(int,int);
 void menu_principal();
 void calculadora_comun();
-void calculadora_cientifica();
-void rellenar_tabla(int, int*,int*);
 int operatoria(char*);
 
 int main(){
@@ -49,40 +44,34 @@ void menu_principal(){
         printf("\n ------------------- \n");
         printf("\n");
         printf("\n ================================== \n");
-        printf("\n |1| -    Calculadora comun   - |1| \n");
-        printf("\n |2| - Calculadora cientifica - |2| \n");
-        printf("\n |3| -          Salir         - |3| \n");
+        printf("\n |1| -     Usar calculadora   - |1| \n");
+        printf("\n |2| -          Salir         - |2| \n");
         printf("\n ================================== \n");
         printf("\n");
         do{
             fflush(stdin);
             printf("Seleccione opcion: ");
             scanf("%i",&op);
-            if(op < 1 || op > 3){
-                printf("\n xx ERROR. INGRESE UNA OPCION VALIDA xx \n");
+            if(op < 1 || op > 2){
+                printf("\n");
+                printf("\n /////////////////////////////////////// \n");
+                printf("\n xx ERROR. INGRESE UNA OPCION VALIDA xx  \n");
+                printf("\n //////////////////////////////////////  \n");
+                printf("\n");
             }
-        } while(op < 1 || op > 3);
+        } while(op < 1 || op > 2);
         switch(op){
             case 1:
                 system("cls");
                 calculadora_comun();
             break;
-
-            case 2:
-                system("cls");
-               // calculadora_cientifica();
-            break;
         }
 
-    } while(op != 3);
+    } while(op != 2);
 }
 
 void calculadora_comun(){
-    int filas = FILAS, columnas = COLUMNAS, resultado,i;
-    static char tabla_caracteres[filas][columnas];
-    char cadena_operatoria[13],car;
-
-    rellenar_tabla(tabla_caracteres,&filas,&columnas);
+    char cadena_operatoria[13],car,op;
 
     do{
         system("cls");
@@ -106,42 +95,52 @@ void calculadora_comun(){
         printf("\n ============================================= \n");
         printf("\n");
 
-        printf("Escriba su operacion aqui abajo: \n");
-        scanf("%s",cadena_operatoria);
-        resultado = operatoria(cadena_operatoria);
+        printf("\n + Escriba su operacion aqui abajo: \n");
+        fgets(cadena_operatoria,sizeof(cadena_operatoria),stdin);
+        cadena_operatoria[strcspn(cadena_operatoria,"\n")] = '\0';
 
+       resultado = operatoria(cadena_operatoria);
 
-    } while();
+        do{
 
-}
-
-void rellenar_tabla(char tabla_caracteres, int *fil,int *col){
-    int i,j;
-
-    for(i = 0; i < *fil; i++){
-        for(j = 0; j < *col; j++){
-            system("cls");
-            printf("\n Ingrese los botones de forma ordenada \n");
             fflush(stdin);
-            printf("\n + Ingrese valor del boton: ");
-            scanf("%c",&tabla_caracteres[i][j]);
-        }
-    }
+            printf("\n Realizar otra operacion? Ingrese < s | n > : ");
+            scanf("%c",&op);
+            if(op != 's' && op != 'n'){
+                printf("\n");
+                printf("\n ///////////////////////// \n");
+                printf("\n x ERROR. INGRESE s O n x  \n");
+                printf("\n ////////////////////////  \n");
+                printf("\n");
+            }
 
-    printf("\n");
-    printf("\n *** TODOS LOS BOTONES LISTOS *** \n");
+        } while(op != 's' && op != 'n');
+
+    } while(op != 'n');
+
 }
+
 
 int operatoria(char *cadena_operatoria){
-    int i, longitud_cadena;
-    char cad_aux[13];
+    char cad_aux[13], char operadores[] = "+,-,*,/", *token;
+    int acum,i;
+
     strcpy(cad_aux,cadena_operatoria);
-    longitud_cadena = strlen(cad_aux);
-    for(i = 0, i < longitud_cadena; i++){
-        if(isalnum(cad_aux[i])){
+
+    token = strtok(cad_aux,operadores);
+
+    i = 0;
+    acum = 0;
+    while(operadores != NULL){
+        if(acum == 0){
+            acum = acum + atoi(token);
+        } else {
+
 
         }
+        token = strtok(NULL,operadores);
     }
+
 
 
 }
